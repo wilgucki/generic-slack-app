9a1c0a390121d6bc40dc8dcd49ba8b831a1911c4
Maciej Wilgucki <mwilgucki@gmail.com>
2020-06-26 19:39:44 +1000

    add logs

--- slack_app/app.py
+++ slack_app/app.py
@@ -23,9 +23,12 @@ def slack_app_handler(event, context):
     else:
         raise ValueError(f'Unknown resource: {event["resource"]}')
 
+    logger.info(f'Received {event["resource"]}')
+
     ssm_client = boto3.client('ssm')
 
     if event['resource'] == '/action-endpoint' and body['event']['type'] == 'url_verification':
+        logger.info('Respond to challenge request')
         token_name = os.environ['SSM_VERIFICATION_TOKEN_NAME']
 
         verification_token = ssm_client.get_parameter(Name=token_name, WithDecryption=True)
@@ -37,6 +40,7 @@ def slack_app_handler(event, context):
     secret_name = os.environ['SSM_SECRET_NAME']
     signing_secret = ssm_client.get_parameter(Name=secret_name, WithDecryption=True)
 
+    logger.info('Verify message signature')
     signature.verify(
         event['headers']['X-Slack-Signature'],
         event['headers']['X-Slack-Request-Timestamp'],
@@ -48,21 +52,22 @@ def slack_app_handler(event, context):
     sns = boto3.client('sns')
     sns.publish(TargetArn=os.environ['SNS_TOPIC_ARN'], Message=json.dumps(body))
 
-    logging.info('acknowledge message')
-
-    return {"statusCode": 200}
+    return {'statusCode': 200}
 
 
 def queue_worker(event, context):
     for record in event['Records']:
         body = json.loads(record['body'])
+        message = json.loads(body['Message'])
 
-        if 'response_url' in body:  # slash command
-            # TODO process message
+        # TODO check if this is the best way of detecting slash commands
+        if 'response_url' in message:  # slash command
+
+            # process message and replace this generic response with something more adequate
             requests.post(
-                body['response_url'],
+                message['response_url'],
                 headers={'Content-type': 'application/json'},
-                data=json.dumps({'text': 'Thanks for your request, we\'ll process it and get back to you.'})
+                data=json.dumps({'text': 'it works!', 'response_type': 'ephemeral'})
             )
         else:
             # TODO send response
